1b1ed9d33969209958257bdb3a89cbc96413a9c0
Dandan MENG <mengdandanno1@163.com>
2017-05-04 14:42:03 +0800

    refine news detail's toolbar layout

--- app/src/main/java/com/ddmeng/zhihudaily/newsdetail/NewsDetailFragment.java
+++ app/src/main/java/com/ddmeng/zhihudaily/newsdetail/NewsDetailFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -77,6 +80,27 @@ public class NewsDetailFragment extends Fragment implements NewsDetailContract.V
     public void initViews() {
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         setHasOptionsMenu(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.news_detail_options_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                break;
+            case R.id.action_star:
+                break;
+            case R.id.action_comment:
+                break;
+            case R.id.action_thumb_up:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
